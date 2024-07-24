245a41d469b25b52cabafa0a2f800c310473768e
Devon Dunham <itisdevondunham@gmail.com>
2024-07-24 18:05:29 -0400

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import Sidebar from "./components/Sidebar/sidebar";
+
+function App() {
+  return (
+    <>
+      <Sidebar></Sidebar>
+    </>
+  );
+}
+
+export default App;
